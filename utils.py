527053e4d44cca173401e17f62dc035ade3eed92
cstorm125 <cebril@gmail.com>
2019-06-01 16:03:03 +0700

    prepare explore for kaggle kernel

--- utils.py
+++ utils.py
@@ -5,6 +5,19 @@ from plotnine import *
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
 
+#######################################################################
+'''
+Snippet for plotnine with thai font by @korakot
+https://gist.github.com/korakot/01d181229b21411b0a20784e0ca20d3d
+'''
+import matplotlib
+# !wget https://github.com/Phonbopit/sarabun-webfont/raw/master/fonts/thsarabunnew-webfont.ttf -q
+# !cp thsarabunnew-webfont.ttf /usr/share/fonts/truetype/
+matplotlib.font_manager._rebuild()
+matplotlib.rc('font', family='TH Sarabun New')
+theme_set(theme_minimal(11, 'TH Sarabun New'))
+#######################################################################
+
 #cleaning
 def check_missing(df):
     per_missing = df.isnull().mean()
@@ -49,9 +62,8 @@ def replace_dict(x, d):
                 return key
     return 'others'
 
-
 #visualization
-def thai_text(font_size=8): 
+def thai_text(font_size=11): 
     #thanks to @korakot for Thai font with plotnine
     return theme(text=element_text(size=font_size,  family='TH Sarabun New'))
 
@@ -60,17 +72,32 @@ def cat_plot(df,col):
          geom_bar(stat='bin', #histogram
                   binwidth=0.5, #histogram binwidth
                   bins=len(df[col].unique())) + #how many bins
-         theme_minimal() + theme(axis_text_x=element_blank())
+         theme(axis_text_x=element_blank())
         )
     return g
 
-def depcat_plot(df,dep,cat, no_outliers=True, geom=geom_boxplot()):
+def numcat_plot(df,num,cat, no_outliers=True, geom=geom_boxplot()):
     if no_outliers:
-        new_df = remove_outliers(df,dep)
+        new_df = remove_outliers(df,num)
     else:
         new_df = df.copy()
-    g = (ggplot(new_df, aes(x=cat,y=dep)) +
-         geom + theme_minimal() 
+    g = (ggplot(new_df, aes(x=cat,y=num)) +
+         geom 
+        )
+    return g
+
+def catcat_plot(df, cat_dep, cat_ind):
+    g = (ggplot(df,aes(x=cat_dep,fill=cat_dep)) + geom_bar() + 
+         theme(axis_text_x = element_blank()) +
+         facet_wrap(f'~{cat_ind}',scales='free_y')) + theme(panel_spacing_x=0.5)
+    return g
+
+def value_dist_plot(df,bins=30):
+    num_m = df.melt()
+    g = (ggplot(num_m,aes(x='value')) +
+         geom_bar(stat='bin', bins=bins) +
+         facet_wrap('~variable', scales='free') + #facetting by variable
+         theme_minimal() + theme(panel_spacing_x=0.5)
         )
     return g
 
@@ -81,14 +108,6 @@ def jointplot(df,col_x, col_y, no_outliers=True, kind='reg'): #'scatter','resid'
     else:
         new_df = df.copy()
     return sns.jointplot(new_df[col_x],new_df[col_y],kind=kind)
-
-def calc_qq(df,col):
-    sample_qs = [(np.percentile(df[col],i)-np.mean(df[col]))/np.std(df[col]) for i in range(5,100,5)]
-    theoretical_qs = [st.norm.ppf(i/100) for i in range(5,100,5)]
-    qq = pd.DataFrame({'sample_q':sample_qs,'theoretical_q':theoretical_qs})
-    reg = LinearRegression(fit_intercept=False).fit(np.array(qq['theoretical_q'])[:,None], 
-                                 np.array(qq['sample_q'])[:,None])
-    return qq, reg
     
 def qq_plot(df,col):
     qq, reg = calc_qq(df,col)
@@ -97,12 +116,24 @@ def qq_plot(df,col):
         geom_abline(slope=1,intercept=0,color='red') + #perfectly normal line
         stat_function(fun=lambda x: x*reg.coef_[0][0]) + #linear estimation
         ggtitle(f'y= {np.round(reg.coef_[0][0],2)} * x')+ #display equation
-        theme_minimal() +
         labs(x='Theoretical Quantiles (normalized)', y='Sample Qunatiles (normalized)'))
     return g
 
+def boxcox_plot(df, col, ls = [i/10 for i in range(-30,31,5)]):
+    lamb_df = boxcox_lamb_df(df[col],ls)
+    g = (ggplot(lamb_df, aes(x='lamb',y='coef',group=1)) + 
+         geom_point() + geom_line())
+    return g
 
 #transformation
+def calc_qq(df,col):
+    sample_qs = [(np.percentile(df[col],i)-np.mean(df[col]))/np.std(df[col]) for i in range(5,100,5)]
+    theoretical_qs = [st.norm.ppf(i/100) for i in range(5,100,5)]
+    qq = pd.DataFrame({'sample_q':sample_qs,'theoretical_q':theoretical_qs})
+    reg = LinearRegression(fit_intercept=False).fit(np.array(qq['theoretical_q'])[:,None], 
+                                 np.array(qq['sample_q'])[:,None])
+    return qq, reg
+
 def boxcox(ser,lamb=0):
     ser+= 1 - ser.min()
     if lamb==0: 
@@ -121,9 +152,3 @@ def boxcox_lamb_df(ser, ls = [i/10 for i in range(-30,31,5)]):
 def boxcox_lamb(ser, ls = [i/10 for i in range(-30,31,5)]):
     df = boxcox_lamb_df(ser,ls)
     return df.lamb[df.coef.idxmax()]
-
-def boxcox_plot(df, col, ls = [i/10 for i in range(-30,31,5)]):
-    lamb_df = boxcox_lamb_df(df[col],ls)
-    g = (ggplot(lamb_df, aes(x='lamb',y='coef',group=1)) + 
-         geom_point() + geom_line() + theme_minimal())
-    return g
